cd181b967d1ef228cb19ffc3cfde649d5c1dc98b
Wei Chen <B007811@federated.fds>
2019-08-05 13:40:07 -0400

    basic setup and deploy to firebase

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'Hello, Wei',
+    description: 'Wei Chen Personal site and blog',
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+          { text: 'External', link: 'https://google.com' },
+        ]
+      }
+  }
